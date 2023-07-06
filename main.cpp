196b423502445954524e485394bbcb188644bbcb
hotwords123 <hotwords123@gmail.com>
2023-07-06 10:46:22 +0800

    add command line arguments

--- main.cpp
+++ main.cpp
@@ -2,43 +2,48 @@
 #include "SudokuSolver.h"
 #include <iostream>
 
-int main() {
+int main(int argc, char **argv) {
     Sudoku sudoku;
     if (!sudoku.readFromFile("sudoku.txt")) {
         return 1;
     }
 
-    // find first 10 solutions of the puzzle
-    auto solve = [&]() {
     SudokuSolver solver(sudoku.data());
-        auto solutions = solver.solve(10);
-        for (const auto &solution : solutions) {
-            solution.print();
-            std::cout << "\n";
-        }
-    };
 
-    // find solutions in a human-like way
-    auto test = [&]() {
-        SudokuSolver solver(sudoku.data());
-        bool ok = true;
-        while (ok) {
-            ok = false;
-            while (solver.reduceOne()) {
-                ok = true;
-            }
-            if (solver.reduceGroup()) {
-                ok = true;
-            }
-            solver.solutionGrid().print();
-            std::cout << "\n";
-        }
-    };
+    std::string param = "10"; // find 10 solutions by default
+    if (argc == 2) {
+        param = argv[1];
+    }
 
-    if (true) {
-        solve();
-    } else {
-        test();
+    if (param == "alt") { // find solutions step by step in a human-like way
+        std::cout << "Solving step by step...\n";
+
+        bool filled = false;
+        while (true) {
+            if (solver.reduceOne()) {
+                filled = true;
+            } else {
+                if (!filled) break;
+                solver.solutionGrid().print();
+                std::cout << "\n";
+                filled = false;
+                if (!solver.reduceGroup()) break;
+            }
+        }
+    } else { // find solutions by depth-first search
+        int limit = std::stoi(param);
+        std::cout << "Solving by depth-first search (limit: " << limit << ")...\n";
+
+        auto solutions = solver.solve(limit);
+        if (solutions.empty()) {
+            std::cout << "No solution found.\n";
+        } else {
+            std::cout << "Found " << solutions.size() << " solution(s).\n";
+            for (const auto &solution : solutions) {
+                solution.print();
+                std::cout << "\n";
+            }
+        }
     }
 
     return 0;
